64effadd66ee8a497accb9add8c864d719d0acef
Adi Eberhard <adi.eberhard@gmail.com>
2026-01-29 20:33:40 +0100

    rearrange

--- install-all.sh
+++ install-all.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-. ./install-megacmd.sh
-. ./install-stow.sh
-. ./install-tmux.sh
-. ./install-hyprland-overrides.sh
-. ./install-ghostty.sh
 . ./install-zsh.sh
+. ./install-megacmd.sh
+. ./install-tmux.sh
+. ./install-ghostty.sh
+
+. ./install-stow.sh
 . ./install-dotfiles.sh
+#. ./install-hyprland-overrides.sh
 
 . ./set-shell.sh
